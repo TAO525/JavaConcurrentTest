719831109199030a69f7d2fb41a39302c123d1d7
gutao <542779063@qq.com>
2018-05-13 13:09:05 +0800

    update

--- src/nio/Server.java
+++ src/nio/Server.java
@@ -23,6 +23,15 @@ public class Server {
         server.configureBlocking(false);//!
         Selector selector = Selector.open();
         server.register(selector, SelectionKey.OP_ACCEPT);
+
+        //尝试多个端口监听
+        ServerSocketChannel server2 = ServerSocketChannel.open();
+        InetSocketAddress inetSocketAddress2 = new InetSocketAddress(8001);
+        server2.bind(inetSocketAddress2);
+//        server2.socket().bind(inetSocketAddress,5);//可以设置tcp accept池
+        server2.configureBlocking(false);//!
+        server2.register(selector,SelectionKey.OP_ACCEPT);
+
         // 轮询式地获取选择器上已经准备就绪的事件
         //selector.select()方法是阻塞的
         while (selector.select()>0) {
@@ -31,7 +40,9 @@ public class Server {
             while (iterator.hasNext()) {
                 SelectionKey key = iterator.next();
                 if(key.isAcceptable()){
-                    SocketChannel client = server.accept();
+//                    SocketChannel client = server.accept();
+                    ServerSocketChannel serverY = (ServerSocketChannel)key.channel();
+                    SocketChannel client = serverY.accept();
                     client.configureBlocking(false);
                     client.register(selector,SelectionKey.OP_READ);
                 }
